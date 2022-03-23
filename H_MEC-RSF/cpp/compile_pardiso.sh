6174094e0d60cb3b65d6906de1f88252b6324877
Fabian Kistler <kistlerf@ethz.ch>
2022-03-23 09:48:55 +0100

    small corrections

--- H_MEC-RSF/cpp/compile_pardiso.sh
+++ H_MEC-RSF/cpp/compile_pardiso.sh
@@ -20,4 +20,11 @@ echo "Compile Program..."
 icpc -std=c++17 -O3 -I/usr/include/eigen3 -DEIGEN_USE_MKL_ALL -DMKL_LP64 -I/intel/mkl/include $MAIN_FILE -L/intel/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -o h_mec_cpp_pardiso
 
 echo "Submit Job..."
-bsub -W "24:00" -R 'rusage[mem=10000]' -R 'select[model=$CPU_TYPE]' -J hmec_cpp -o output.txt ./h_mec_cpp_pardiso
+if [ -z "$CPU_TYPE" ]
+then
+	echo "No CPU Type specified..."
+	bsub -W "24:00" -R "rusage[mem=10000]" -J hmec_cpp -o output.txt ./h_mec_cpp_pardiso
+else
+	echo "Only using $CPU_TYPE"
+	bsub -W "24:00" -R "rusage[mem=10000]" -R "select[model=$CPU_TYPE]" -J hmec_cpp -o output.txt ./h_mec_cpp_pardiso
+fi
